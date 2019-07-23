22be2193e444324672d0bd60abe062e34af0b9f5
me <eedoshapira@gmail.com>
2019-07-24 01:05:28 +0300

    okok

--- derwiderstand.py
+++ derwiderstand.py
@@ -12,7 +12,7 @@ def commit():
 @bot.command()
 async def register(ctx, arg):
     if arg not in db['players']:
-        db['players'][arg] = {'name': arg, 'score': 0, 'wins': 0, 'losses': 0}
+        db['players'][arg] = {'name': arg, 'score': 0, 'spy_wins': 0, 'spy_losses': 0, 'resistance_wins': 0, 'resistance_losses': 0, 'merlin_dodges': 0, 'merlin_games': 0, 'merlin_caught': 0, 'merlin_spygames': 0, 'rogue_wins': 0, 'rogue_games': 0}
         commit()
         await ctx.send('added new player: %s' % arg)
     else:
@@ -21,7 +21,21 @@ async def register(ctx, arg):
 @bot.command()
 async def showscore(ctx):
     sort = sorted(db['players'], key=lambda x: -db['players'][x]['score'])
-    await ctx.send('current standings:\n[points // win rate]\n-------------\n'+'\n'.join('%s. %s\n[%s // %.2f'%(n+1,i,db['players'][i]['score'],100.0*db['players'][i]['wins']/(0.0000000000000000000000000001+db['players'][i]['wins']+db['players'][i]['losses']))+'%]\n-------------' for n,i in enumerate(sort)))
+    msg = 'current standings:\n```[ points | win rate | games | spy wins/games | res wins/games | merlin wins/games | merlin caught/games vs merlin | rogue wins/games ]```\n'
+    for n,i in enumerate(sort):
+        p = db['players'][i]
+        points = p['score']
+        total_winpercent = 100.0*(p['resistance_wins']+p['spy_wins'])/(0.0000000000000000000000000001+p['resistance_wins']+p['spy_losses']+p['spy_wins']+p['resistance_losses'])
+        games = db['players'][i]['spy_wins']+db['players'][i]['resistance_wins']+db['players'][i]['spy_losses']+db['players'][i]['resistance_losses']
+        spy_stats = '%s/%s'%(p['spy_wins'], p['spy_wins']+p['spy_losses'])
+        resistance_stats = '%s/%s'%(p['resistance_wins'], p['resistance_wins']+p['resistance_losses'])
+        merlin_dodges = '%s/%s' % (p['merlin_dodges'], p['merlin_games'])
+        merlin_caught = '%s/%s' % (p['merlin_caught'], p['merlin_spygames'])
+        rogue_stats = '%s/%s' % (p['rogue_wins'], p['rogue_games'])
+        
+        msg += '```%s. %s\n[ %s | %.2f'%(n+1, i, points, total_winpercent)+'% | '+' | '.join(str(i) for i in [games, spy_stats, resistance_stats, merlin_dodges, merlin_caught, rogue_stats])+' ]```\n'
+
+    await ctx.send(msg)
     
 @bot.command()
 async def newgame(ctx, *args):
@@ -96,39 +110,83 @@ async def newgame(ctx, *args):
             for i in final_roles:
                 if 'rogue' in final_roles[i] and 'spy' in final_roles[i]:
                     db['players'][i]['score'] += 1
+                    db['players'][i]['spy_wins'] += 1
+                    db['players'][i]['rogue_wins'] += 1
+                    db['players'][i]['rogue_games'] += 1
+                    
+                    
+                    
                     winners.append(i)
+                elif 'spy' in final_roles[i]:
+                    db['players'][i]['spy_losses'] += 1
+                else:
+                    db['players'][i]['resistance_losses'] += 1
         elif 'resistance' in final_result:
             for i in final_roles:
                 if 'rogue' in final_roles[i] and 'resistance' in final_roles[i]:
                     db['players'][i]['score'] += 1
+                    db['players'][i]['resistance_wins'] += 1
+                    db['players'][i]['rogue_wins'] += 1
+                    db['players'][i]['rogue_games'] += 1
+                    
                     winners.append(i)
+                elif 'resistance' in final_roles[i]:
+                    db['players'][i]['resistance_losses'] += 1
+                else:
+                    db['players'][i]['spy_losses'] += 1
     elif 'resistance' in final_result:
         for i in final_roles:
                 if 'resistance' in final_roles[i]:
                     if 'rogue' in final_roles[i]:
                         db['players'][i]['score'] += 0.5
+                        db['players'][i]['rogue_games'] += 1
                     else:
                         db['players'][i]['score'] += 1
+                    db['players'][i]['resistance_wins'] += 1
+                    if 'merlin' in final_roles[i]:
+                        db['players'][i]['merlin_dodges'] += 1
+                    
                     winners.append(i)
+                else:
+                    db['players'][i]['spy_losses'] += 1
     elif 'spy' in final_result:
         for i in final_roles:
                 if 'spy' in final_roles[i]:
                     db['players'][i]['score'] += 1
+                    db['players'][i]['spy_wins'] += 1
+                    if 'merlin' in final_result:
+                        db['players'][i]['merlin_caught'] += 1
                     winners.append(i)
+                else:
+                    
+                    db['players'][i]['resistance_losses'] += 1
+
+    count_merlin = 0
+    for i in final_roles:
+        if 'merlin' in final_roles[i]:
+            count_merlin = 1
+            db['players'][i]['merlin_games'] += 1
+            break
+    if count_merlin:
+        for i in final_roles:
+            if 'spy' in final_roles[i]:
+                db['players'][i]['merlin_spygames'] += 1
+    
+                    
 
     losers = [i for i in final_roles if i not in winners]
 
     #lists needed for rating calc later..
-    for i in winners:
-        db['players'][i]['wins'] += 1
-    for i in losers:
-        db['players'][i]['losses'] += 1
-    
+##    for i in winners:
+##        db['players'][i]['wins'] += 1
+##    for i in losers:
+##        db['players'][i]['losses'] += 1
+    commit()
     await ctx.send('winners: %s\nlosers: %s' % (', '.join(['[%s]'%i for i in winners]), ', '.join(['[%s]'%i for i in losers])))
 
 
     
-    commit()
+    
     
     
     
